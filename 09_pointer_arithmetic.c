85878dd463fa544931a6cfed5f6c66a29f9701b5
praveen k <praveenk53ngup@gmail.com>
2025-11-25 08:18:41 +0530

    Add files via upload

---
+++ 09_pointer_arithmetic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int arr[] = {5,10,15,20,25};
+    int *ptr = arr;
+    printf("ptr = %p,*ptr = %d\n", ptr , *ptr); 
+    ptr++ ; //move to next element
+    printf("after ptr++ : ptr = %p, *ptr = %d\n", ptr , *ptr);
+    ptr++ ; //move 2 steps
+    printf("after ptr +=2: ptr =%p, *ptr = %d\n", ptr , *ptr);
+    return 0;
+}
